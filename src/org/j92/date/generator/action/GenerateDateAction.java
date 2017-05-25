0152b9d9905ab902b32e843762adacc863eeec75
Joost van Driel <joostvdriel@gmail.com>
2017-05-25 10:04:59 +0200

    Added multi caret support

--- src/org/j92/date/generator/action/GenerateDateAction.java
+++ src/org/j92/date/generator/action/GenerateDateAction.java
@@ -4,6 +4,7 @@ import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.actionSystem.CommonDataKeys;
 import com.intellij.openapi.command.WriteCommandAction;
+import com.intellij.openapi.editor.Caret;
 import com.intellij.openapi.editor.Document;
 import com.intellij.openapi.editor.Editor;
 import com.intellij.openapi.editor.SelectionModel;
@@ -16,23 +17,26 @@ public class GenerateDateAction extends AnAction {
         final Project project = event.getData(CommonDataKeys.PROJECT);
         final Editor editor = event.getData(CommonDataKeys.EDITOR);
 
-        if (null == editor) {
+        if (null == editor || project == null) {
             return;
         }
 
-        Document document = editor.getDocument();
-
-        final SelectionModel selectionModel = editor.getSelectionModel();
-
         String formattedDate = new CurrentDateGenerator().generate();
 
-        final int selectionStart = selectionModel.getSelectionStart();
-        final int selectionEnd = selectionModel.getSelectionEnd();
-
-        Runnable runnable = () -> document.replaceString(selectionStart, selectionEnd, formattedDate);
+        Runnable runnable = () -> {
+            for (Caret caret : editor.getCaretModel().getAllCarets()) {
+                insertTextAtCaret(editor.getDocument(), caret, formattedDate);
+            }
+        };
 
         WriteCommandAction.runWriteCommandAction(project, runnable);
+    }
 
-        editor.getCaretModel().moveToOffset(selectionStart + formattedDate.length());
+    private static void insertTextAtCaret(Document document, Caret caret, CharSequence text) {
+        document.replaceString(caret.getSelectionStart(), caret.getSelectionEnd(), text);
+        if (caret.hasSelection()) {
+            caret.setSelection(caret.getSelectionStart(),  caret.getSelectionStart() + text.length());
+        }
+        caret.moveToOffset(caret.getSelectionStart() + text.length());
     }
 }
